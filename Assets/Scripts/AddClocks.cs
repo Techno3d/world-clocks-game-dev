1fac2dd45dd46dafc00570bb64bb8792bc1daa57
Shadman Syed <shadman3d@gmail.com>
2024-09-26 23:25:03 -0400

    Some small changed

--- Assets/Scripts/AddClocks.cs
+++ Assets/Scripts/AddClocks.cs
@@ -14,20 +14,23 @@ public class AddClocks : MonoBehaviour
     [SerializeField]
     string[] zoneNames = new string[]{"", "", "", "", ""};
     [SerializeField]
-    float[] timeZones = new float[]{0, -5, 6, 4, 2 };
+    string[] timeZones = new string[]{};
     List<GameObject> clocks = new();
     // Start is called before the first frame update
     void Start()
     {
         int i = 0;
         float initOffset = columns*seperation/4f;
-        foreach(float zone in timeZones)
+        foreach(string zone in timeZones)
         {
             int curRow = i/columns;
             int curColumn = i%columns;
-            GameObject clock = Instantiate(funny, new Vector3(curColumn*seperation - initOffset, -curRow*(seperation+5) + 10, 0), Quaternion.identity, this.transform);
+            if(i/columns == timeZones.Length/columns && timeZones.Length%columns!=0) {
+                initOffset = columns*seperation/4f - seperation/2f;
+            }
+            GameObject clock = Instantiate(funny, new Vector3(curColumn*seperation - initOffset, -curRow*(seperation+5) + 10, 0) + transform.position, Quaternion.identity, transform);
             Clock clockcode = clock.GetComponent<Clock>();
-            clockcode.UtcOffset = zone;
+            clockcode.IanaCode = zone;
             clockcode.text = zoneNames[i];
             clocks.Add(clock);
             i++;
--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -9,6 +9,10 @@ public class Clock : MonoBehaviour
     [SerializeField]
     GameObject clock;
     [SerializeField]
+    GameObject hoursHand;
+    [SerializeField]
+    GameObject minutesHand;
+    [SerializeField]
     Transform hoursPivot;
     [SerializeField]
     Transform minutePivot;
@@ -16,7 +20,7 @@ public class Clock : MonoBehaviour
     Transform secondPivot;
     [SerializeField]
     private TextMeshPro nameText;
-    public float UtcOffset = 0;
+    public string IanaCode = "UTC";
     public string text;
     float hoursToDegree = -30f, minutesToDegrees = -6f, secondsToDegrees = -6f;
     
@@ -28,16 +32,25 @@ public class Clock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        TimeSpan time = DateTime.UtcNow.TimeOfDay;
-        hoursPivot.localRotation = Quaternion.Euler(0f, 0f, hoursToDegree * (float)(time.TotalHours+UtcOffset));
-        minutePivot.localRotation = Quaternion.Euler(0f, 0f, minutesToDegrees * (float)time.TotalMinutes);
-        secondPivot.localRotation = Quaternion.Euler(0f, 0f, secondsToDegrees * (float)time.TotalSeconds);
-        nameText.text = text + "\n" + (time.Hours+UtcOffset) + ":" + time.Minutes + ":" + time.Seconds;
-        if (time.TotalHours + UtcOffset > 0)
+        DateTime time = DateTime.UtcNow;
+        TimeZoneInfo current = TimeZoneInfo.FindSystemTimeZoneById(IanaCode);
+        time = TimeZoneInfo.ConvertTime(time, current);
+        TimeSpan timeSpan = time.TimeOfDay;
+        hoursPivot.localRotation = Quaternion.Euler(0f, 0f, hoursToDegree * (float)timeSpan.TotalHours);
+        minutePivot.localRotation = Quaternion.Euler(0f, 0f, minutesToDegrees * (float)timeSpan.TotalMinutes);
+        secondPivot.localRotation = Quaternion.Euler(0f, 0f, secondsToDegrees * (float)timeSpan.TotalSeconds);
+        nameText.text = text + "\n" + (timeSpan.Hours/10==0? "0"+timeSpan.Hours : timeSpan.Hours) + 
+            ":" + (timeSpan.Minutes/10==0 ? "0"+timeSpan.Minutes : timeSpan.Minutes) + 
+            ":" + (timeSpan.Seconds/10==0 ? "0" + timeSpan.Seconds : timeSpan.Seconds);
+        if (timeSpan.TotalHours> 0 && timeSpan.TotalHours < 6 || timeSpan.TotalHours > 20)
         {
             clock.GetComponent<MeshRenderer>().material.color = Color.black;
+            hoursHand.GetComponent<MeshRenderer>().material.color = Color.white;
+            minutesHand.GetComponent<MeshRenderer>().material.color = Color.white;
         } else {
             clock.GetComponent<MeshRenderer>().material.color = Color.white;
+            hoursHand.GetComponent<MeshRenderer>().material.color = Color.black;
+            minutesHand.GetComponent<MeshRenderer>().material.color = Color.black;
         }
     }
 }
